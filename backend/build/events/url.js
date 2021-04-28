af801afcdc38bfb402ac103b2f9f9a0019e7a406
M Ahmed Mushtaq <mahmedmushtaq296@gmail.com>
2021-04-29 01:12:32 +0500

    tsconfig file is updated

--- backend/build/events/url.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UPDATE_URL_ENDPOINT = exports.REGISTRATION_URL_ENDPOINT = void 0;
-exports.REGISTRATION_URL_ENDPOINT = process.env.MESSAGES_SERVER_URL + "/register";
-exports.UPDATE_URL_ENDPOINT = process.env.MESSAGES_SERVER_URL + "/user/update";
